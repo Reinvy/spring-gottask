a2ab4298910f31e4ccd9b2920fe3f47878a6d04b
Reinvy <satriaoner@gmail.com>
2024-03-25 16:12:55 +0700

    first commit

---
+++ src/main/java/tech/reinvy/gottask/GottaskApplication.java
@@ -0,0 +1,13 @@
+package tech.reinvy.gottask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GottaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GottaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/reinvy/gottask/GottaskApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.reinvy.gottask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GottaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
